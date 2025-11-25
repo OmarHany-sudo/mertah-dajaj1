cf4727c3753e43d51a4d3e5cb6f592cb79025a55
Omar hany shalaby <S23511739EG@Std.aou.edu.eg>
2025-11-25 03:16:40 +0200

    Update page.tsx

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -1,11 +1,15 @@
+"use client";
+
 import { Navbar } from "@/components/Navbar";
 import { OrderForm } from "@/components/OrderForm";
 
 export default function OrderPage() {
   return (
-    <div className="max-w-4xl mx-auto p-4">
+    <div className="min-h-screen max-w-4xl mx-auto pb-10">
       <Navbar />
-      <OrderForm />
+      <main className="px-4 md:px-8 pb-8">
+        <OrderForm />
+      </main>
     </div>
   );
 }
